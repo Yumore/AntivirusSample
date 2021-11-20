18dc69213a71c07223ca21811a2718e05aa287a3
Nathaniel <nathanwriting@126.com>
2021-11-20 12:04:55 +0800

    解决日志递归调用的问题

--- utility/src/main/java/com/nathaniel/utility/LoggerUtils.java
+++ utility/src/main/java/com/nathaniel/utility/LoggerUtils.java
@@ -12,37 +12,32 @@ import com.google.gson.Gson;
  */
 public final class LoggerUtils {
     public static final String TAG = LoggerUtils.class.getSimpleName();
-    private static final Gson GSON = new Gson();
 
     public static <T> void logger(T t) {
         logger(Level.DEBUG, t);
-    }
-
-    public static <T> void logger(Level level, T t) {
-        if (t instanceof CharSequence) {
-            logger(level, t);
-        } else {
-            logger(level, GSON.toJson(t));
-        }
     }
 
     @SafeVarargs
     public static <T> void logger(T... ts) {
         StringBuilder stringBuilder = new StringBuilder();
         if (ts == null || ts.length == 0) {
-            logger("logger message is empty in " + LoggerUtils.class.getSimpleName());
-            return;
-        }
-        for (int i = 0; i < ts.length; i++) {
-            stringBuilder.append(new Gson().toJson(ts[i]));
-            if (i < ts.length - 1) {
-                stringBuilder.append(" - ");
+            stringBuilder.append("logger message is empty in ").append(TAG);
+        } else {
+            for (int i = 0; i < ts.length; i++) {
+                stringBuilder.append(new Gson().toJson(ts[i]));
+                if (i < ts.length - 1) {
+                    stringBuilder.append(" - ");
+                }
             }
         }
         logger(stringBuilder.toString());
     }
 
-    private static void logger(Level level, String message) {
+    public static <T> void logger(Level level, T t) {
+        dealLogger(level, new Gson().toJson(t));
+    }
+
+    private static void dealLogger(@NonNull Level level, String message) {
         if (EmptyUtils.isEmpty(message)) {
             message = "logger message is empty in " + LoggerUtils.class.getSimpleName();
         }
@@ -53,17 +48,17 @@ public final class LoggerUtils {
         int passage = (message.length() + maxLength) / maxLength;
         for (int index = 0; index < passage; index++) {
             if (length > ending) {
-                logged(LoggerUtils.TAG, level, message.substring(started, ending));
+                realLogged(TAG, level, message.substring(started, ending));
                 started = ending;
                 ending = ending + maxLength;
             } else {
-                logged(LoggerUtils.TAG, level, message.substring(started, length));
+                realLogged(TAG, level, message.substring(started, length));
                 break;
             }
         }
     }
 
-    private static void logged(@NonNull String tag, @NonNull Level level, @NonNull String message) {
+    private static void realLogged(@NonNull String tag, @NonNull Level level, @NonNull String message) {
         if (!BuildConfig.DEBUG) {
             return;
         }
